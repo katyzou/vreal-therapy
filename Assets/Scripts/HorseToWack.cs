37033024bb575b491aebcfc4db39e76d02089edb
andrewhongnguyen <andrewhongnguyen@gmail.com>
2017-02-26 16:49:09 -0800

    redid mole game

---
+++ Assets/Scripts/HorseToWack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorseToWack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void OnMouseDown()
+    {
+        Destroy(this.gameObject);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        /*if(other.gameObject.GetComponent<controller>())
+         * {
+         *      Destroy(this.gameObject);
+         * }
+         * */
+    }
+}
