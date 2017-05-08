632d8cbc60e50528e4cdd48a714dbeeb5bb6a05a
Andrew H Nguyen <ahn064@UCSD.EDU>
2017-05-07 17:30:10 -0700

    more iceland stuff

--- Assets/Scripts/Snow.cs
+++ Assets/Scripts/Snow.cs
@@ -11,7 +11,7 @@ public class Snow : MonoBehaviour {
     {
         Vector3 temp = Input.mousePosition;
         temp.z = 30f; // Set this to be the distance you want the object to be placed in front of the camera.
-        this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+        this.transform.position = mainCamera.ScreenToWorldPoint(temp);
 
     }
     private void OnMouseEnter()
