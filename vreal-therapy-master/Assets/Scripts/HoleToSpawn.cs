c05aaa6e57eee85a29f921b6bd481104045de40a
vintara <vintara@UCSD.EDU>
2017-05-13 15:10:07 -0700

    got the files

---
+++ vreal-therapy-master/Assets/Scripts/HoleToSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleToSpawn : MonoBehaviour {
+
+    public GameObject horse;
+    Quaternion spawnRotation = Quaternion.Euler(0, 0, 0);
+
+    public void Spawn()
+    {
+        Vector3 spawnPosition = new Vector3(this.gameObject.transform.position.x, 
+        this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+        Instantiate(horse, spawnPosition, spawnRotation);
+    }
+}
